f6aa1cfd033badcbbd662abd1ffb2029ad1dfa2d
CMB <56138926+catherine-m-breen@users.noreply.github.com>
2024-07-15 13:43:46 -0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,15 +1,5 @@
 import torch
 import os
-# constant paths
-
-# manual_labels_path = '/Users/catherinebreen/Documents/Chapter1/WRRsubmission/SNEX20_SD_TLI_clean.csv'
-
-# ROOT_PATH = '/Volumes/CatBreen/CV4ecology/SNEX20_TLI_resized_clean'
-# OUTPUT_PATH = '/Users/catherinebreen/Documents/Chapter1/aurora_outputsJul26/snow_poles_outputs_resized_FT10p_LRe4_BS64_E100_clean_OKonly'
-# snowfreetbl_path = '/Users/catherinebreen/Documents/Chapter1/WRRsubmission/snowfree_table.csv'
-# manual_labels_path = '/Users/catherinebreen/Documents/Chapter1/WRRsubmission/manuallylabeled_CUBM02corr.csv'
-# datetime_info = '/Users/catherinebreen/Documents/Chapter1/WRRsubmission/labeledImgs_datetime_info.csv'
-# native_res_path = '/Users/catherinebreen/Documents/Chapter1/WRRsubmission/nativeRes.csv'
 
 ROOT_PATH = '/datadrive/vmData/SNEX20_TLI_resized_clean'
     #OUTPUT_PATH = '/datadrive/vmData/snow_poles_outputs_resized_FT_5_LRe4_BS64_E100_clean_Aug' #snow_poles_outputs_resized_FT_10_LRe4_BS64_E100_clean'
@@ -19,8 +9,6 @@ snowfreetbl_path = '/datadrive/vmData/snowfree_table.csv'
 manual_labels_path = '/datadrive/vmData/manuallylabeled_CUBM02corr.csv' #'/datadrive/vmData/SNEX20_SD_TLI_clean.csv'
 datetime_info = '/datadrive/vmData/labeledImgs_datetime_info.csv' #'/datdrive/vmData/native_res/native_res'
 native_res_path = '/datadrive/vmData/nativeRes.csv'
-
-#OUTPUT_PATH = '/Users/catherinebreen/Documents/Chapter1/dendrite_outputs/IN/snow_poles_outputs_resized_LRe4_BS64_clean_wWAOK_IN'
 
 # learning parameters
 BATCH_SIZE = 64 #64 #4 #32
