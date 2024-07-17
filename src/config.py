ab1cd0aba4b7acff8b98e1ce6d442f18251c291b
CMB <56138926+catherine-m-breen@users.noreply.github.com>
2024-07-17 17:10:26 -0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,12 +3,12 @@ import os
 
 ### THESE PATHS ARE FOR THE DEMO DATA; PLEASE UPDATE WITH OWN PATHS"
 ### All in snowpoles_data
-ROOT_PATH = '~/snowpoles_data/demo_data'
-OUTPUT_PATH = '~/snowpoles_data/model'
+ROOT_PATH = '~/example_data'
+OUTPUT_PATH = '~/output'
 snowfreetbl_path = '~/snowpoles_data/demo_data/snowfree_table.csv'
-manual_labels_path = '~/snowPoles_labels_clean_448res' 
-datetime_info = '~/labeledImgs_datetime_info.csv' 
-native_res_path = '~/nativeRes.csv'
+manual_labels_path = '~/example_data/snowPoles_labels_clean_448res.csv' 
+# datetime_info = '~/labeledImgs_datetime_info.csv' 
+# native_res_path = '~/nativeRes.csv'
 
 # learning parameters
 BATCH_SIZE = 64 
@@ -16,7 +16,7 @@ LR = 0.0001
 EPOCHS = 10000 #100
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # train/test split
-TEST_SPLIT = 0.2  ## could update for the cameras that we want to hold out as validation
+TEST_SPLIT = 0.2 
 # show dataset keypoint plot
 SHOW_DATASET_PLOT = False
 AUG = False 
@@ -25,6 +25,6 @@ keypointColumns = ['x1', 'y1', 'x2', 'y2'] ## update
 
 # Fine-tuning set-up
 FINETUNE = False 
-FT_PATH = '/datadrive/vmData/snow_poles_outputs_resized_LRe4_BS64_E100_clean_SNEX_IN' ## model that you want to fine tune
+FT_PATH = '~/model/model.pth' ## model that you want to fine tune
 FT_sample = 10
-FT_IMG_PATH = '/datadrive/vmData/WAsubset_every10'
+FT_IMG_PATH = '~/example_data'
