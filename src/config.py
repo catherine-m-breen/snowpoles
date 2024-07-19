57cc04ede2b40e814d5570a955b76e32495a71ff
CMB <56138926+catherine-m-breen@users.noreply.github.com>
2024-07-19 16:39:46 -0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,30 +1,26 @@
 import torch
 import os
 
-### THESE PATHS ARE FOR THE DEMO DATA; PLEASE UPDATE WITH OWN PATHS"
+### THESE PATHS ARE FOR THE DEMO NONTRAINED DATA; PLEASE UPDATE WITH OWN PATHS"
 ### All in snowpoles_data
-ROOT_PATH = '~/example_data'
-OUTPUT_PATH = '~/output'
-snowfreetbl_path = '~/snowpoles_data/demo_data/snowfree_table.csv'
-manual_labels_path = '~/example_data/snowPoles_labels_clean_448res.csv' 
-# datetime_info = '~/labeledImgs_datetime_info.csv' 
-# native_res_path = '~/nativeRes.csv'
+ROOT_PATH = './example_nontrained_data'
+OUTPUT_PATH = './output1'  ## the folder where you want to store your custom model
+metadata = './example_nontrained_data/pole_metadata.csv'
+labels = './example_nontrained_data/labels.csv'
 
 # learning parameters
 BATCH_SIZE = 64 
 LR = 0.0001
-EPOCHS = 10000 #100
+EPOCHS = 20 #1000
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # train/test split
 TEST_SPLIT = 0.2 
 # show dataset keypoint plot
 SHOW_DATASET_PLOT = False
-AUG = False 
+AUG = True 
 
 keypointColumns = ['x1', 'y1', 'x2', 'y2'] ## update
 
 # Fine-tuning set-up
-FINETUNE = False 
-FT_PATH = '~/model/model.pth' ## model that you want to fine tune
-FT_sample = 10
-FT_IMG_PATH = '~/example_data'
+FINETUNE = True 
+FT_PATH = './models/CO_and_WA_model.pth' 
