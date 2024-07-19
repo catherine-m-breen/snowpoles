b9646a398471fe3646abded1fa47902a49c1b7f6
CMB <56138926+catherine-m-breen@users.noreply.github.com>
2024-07-19 13:53:25 -0400

    Update depth_conversion.py

--- src/depth_conversion.py
+++ src/depth_conversion.py
@@ -1,85 +1,63 @@
 '''
-load model results and identify snow depth for AVERAGE USER GITHUB 
-REPOSITORY
+written by Catherine Breen 
+June 2024
 
-Catherine Breen
-catherine.m.breen@gmail.com
+If after the predictions you want to predict snow depth again
+such as if you have improved metadata, you can run this script by itself on the predictions and the metadata
+
+example command line to run:
+
+python src/depth_conversion.py
 
 '''
 
-import torch
+
 import numpy as np
-import cv2
-import albumentations  ## may need to do pip install
-import config
 from model import snowPoleResNet50
 import argparse
-import glob
-import IPython
-import utils
 import pandas as pd
 from tqdm import tqdm
 from scipy.spatial import distance
-import os
-import matplotlib.pyplot as plt
-from scipy.spatial import distance
-from PIL import Image
 
 def main():
     # Argument parser for command-line arguments:
     parser = argparse.ArgumentParser(description='Predict top and bottom coordinates.')
-    parser.add_argument('--predictions_path', required=True, help='Path to camera image directory', default = '/predictions')
-    parser.add_argument('--snwfreetbl_path,', required=True, help='Path to snow free table', default = '/snowfree_table.csv')
-    parser.add_argument('--native_res_info', required=True, help='Path to example image folder', default = '/snowfree_table.csv')
+    parser.add_argument('--predictions_path', required=True, help='Path to camera image directory', default = '/predictions/results.csv')
+    parser.add_argument('--metadata', required=False, help='Path to pole metadata', default = "/example_data/pole_metadata.csv")
+    parser.add_argument('')
     args = parser.parse_args()
 
-    data = pd.read_csv(f'{args.predictions_path}/eval/results.csv')
-    snwfreetbl = pd.read_csv(f'{args.snwfreetbl_path}/snowfree_table.csv') 
-    nativeRes_imgs = glob.glob(f'{args.native_res_info}/resolution_info/*')
-
-    camIDs = []
-    nativeRes = []
-
-    ## turn into dictionary 
-    for img in nativeRes_imgs:
-        camID = img.split('/')[-1].split('_')[0]
-        image = cv2.imread(img)
-        orig_h, orig_w, channel = image.shape
-        camIDs.append(camID)
-        nativeRes.append([orig_h, orig_w])
-
-    resDic = dict(zip(camIDs, nativeRes))
+    predictions = pd.read_csv(f'{args.predictions_path}')
+    metadata = pd.read_csv(f"{args.metadata}")
 
     files = []
-    cam = []
-    length_cm = []
-    sd = []
+    cameras =[]
+    snow_depths = []
 
-    for filename in data['filename']:
+    for filename in predictions['filename']:
         try: 
-            camera = filename.split('_')[0]
-            res = resDic[camera]
-            conversion = snwfreetbl.loc[snwfreetbl['camera'] == camera, 'conversion'].iloc[0]
-            snwfreestake = snwfreetbl.loc[snwfreetbl['camera'] == camera, 'snow_free_cm'].iloc[0]
+            camera = filename.split('/')[-1]
+        
+            full_length_pole_cm = metadata.loc[metadata['camera_id'] == camera, 'pole_length'].iloc[0]
+            pixel_cm_conversion = metadata.loc[metadata['camera_id'] == camera, 'pixel_cm_conversion'].iloc[0]
             #IPython.embed()
             ## need to scale back up 
-            x1 = data.loc[data['filename'] == filename, 'x1_pred'].iloc[0] * (res[1] / 224)
-            y1 = data.loc[data['filename'] == filename, 'y1s_pred'].iloc[0] * (res[0] / 224)
-            x2 = data.loc[data['filename'] == filename, 'x2_pred'].iloc[0] * (res[1] / 224)
-            y2 = data.loc[data['filename'] == filename, 'y2_pred'].iloc[0] * (res[0] / 224)
+            x1 = predictions.loc[predictions['filename'] == filename, 'x1_pred'].iloc[0] 
+            y1 = predictions.loc[predictions['filename'] == filename, 'y1_pred'].iloc[0] 
+            x2 = predictions.loc[predictions['filename'] == filename, 'x2_pred'].iloc[0] 
+            y2 = predictions.loc[predictions['filename'] == filename, 'y2_pred'].iloc[0] 
 
-            pixelLengths = distance.euclidean([x1,y1],[x2,y2])
-            cmLengths = pixelLengths * float(conversion)
-            snowdepth = snwfreestake - cmLengths
+            total_length_pixel = distance.euclidean([x1,y1],[x2,y2])
+            snow_depth = full_length_pole_cm - (pixel_cm_conversion * total_length_pixel)
+            
+            files.append(filename)
+            cameras.append(camera)
+            snow_depths.append(snow_depth)
 
-            #if snowdepth < 0: snowdepth = 0 # so that we reduce the noise
-
-            files.append(filename), 
-            cam.append(camera), length_cm.append(cmLengths), sd.append(snowdepth)
         except: pass
 
-    df = pd.DataFrame({'camera': cam, 'filename': files, 'cmLengths': length_cm,'snowDepth':sd})
-    df.to_csv(f'{args.predictions_path}/results_cm.csv')
+    df = pd.DataFrame({'camera_id': cameras, 'filename': files, 'snowdepth':snow_depths})
+    df.to_csv(f'{args.predictions_path}/results_wsnowdepthcm.csv')
 
 if __name__ == '__main__':
     main()
