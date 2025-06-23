03c42ed42436a12ed84d076c00ae37b3ff6063ba
Nesitive <wbeau12@yahoo.com>
2025-06-23 09:32:05 -0500

    Revert the merge of "main" into "labeling-enhancements" (no, you're not reading those branch names the wrong way)
    This reverts commit edaf4e403f7ddccda0b891d9f67b5a7812803433, reversing
    changes made to 4a4ca4ef0a97f147058550d5935f9ab8b729b40e.

--- src/config.py
+++ src/config.py
@@ -3,25 +3,25 @@ import os
 
 ### THESE PATHS ARE FOR THE DEMO NONTRAINED DATA; PLEASE UPDATE WITH OWN PATHS"
 ### All in snowpoles_data
-ROOT_PATH = "./nontrained_data"
-OUTPUT_PATH = "./output1"  ## the folder where you want to store your custom model
-metadata = "./nontrained_data/pole_metadata.csv"
-labels = "./nontrained_data/labels.csv"
+ROOT_PATH = './example_nontrained_data'
+OUTPUT_PATH = './output1'  ## the folder where you want to store your custom model
+metadata = './example_nontrained_data/pole_metadata.csv'
+labels = './example_nontrained_data/labels.csv'
 
 # learning parameters
-BATCH_SIZE = 64
+BATCH_SIZE = 64 
 LR = 0.0001
-EPOCHS = 20  # 1000
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+EPOCHS = 20 #1000
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # train/test split
-TEST_SPLIT = 0.2
+TEST_SPLIT = 0.2 
 # show dataset keypoint plot
 SHOW_DATASET_PLOT = False
-AUG = True
+AUG = True 
 
-keypointColumns = ["x1", "y1", "x2", "y2"]  ## update
+keypointColumns = ['x1', 'y1', 'x2', 'y2'] ## update
 
 # Fine-tuning set-up
 ## make sure to download model from Zenodo
-FINETUNE = True
-FT_PATH = "./models/CO_and_WA_model.pth"
+FINETUNE = True 
+FT_PATH = './models/CO_and_WA_model.pth' 
--- src/labeling.py
+++ src/labeling.py
@@ -1,4 +1,4 @@
-"""
+'''
 written by Catherine M. Breen 
 cbreen@uw.edu 
 
@@ -12,15 +12,14 @@ x2,y2 = bottom
 
 The labels.csv file can then be directly pointed at train.py for fine-tuning. The user can then run predict.py to extract the snow depth.
 
-example run (double quotes will work on linux or windows)
+example run 
 
-python src/labeling.py --datapath "nontrained_data" --pole_length "304.8" --subset_to_label "2"
-python src/labeling.py --datapath "F:\FDLTCC_photos" --pole_length "152.4" --subset_to_label "100"
+python src/labeling.py --datapath 'nontrained_data' --pole_length '304.8' --subset_to_label '2'
 
-"""
+'''
 
 import cv2
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 import glob
 import argparse
 import tqdm
@@ -36,30 +35,22 @@ from pathlib import Path
 def main():
 
     # Argument parser for command-line arguments:
-    parser = argparse.ArgumentParser(description="Label snowpole images")
-    parser.add_argument("--datapath", help="Path to image dir")
-    parser.add_argument(
-        "--pole_length", help="Length of pole in cm", default="100"
-    )  ### assumes poles are 1 m / 100 cm tall
-    parser.add_argument(
-        "--subset_to_label", help="# of images per camera to label", default="10"
-    )
+    parser = argparse.ArgumentParser(description='Label snowpole images')
+    parser.add_argument('--datapath', help='Path to image dir')
+    parser.add_argument('--pole_length', help='Length of pole in cm', default = '100') ### assumes poles are 1 m / 100 cm tall 
+    parser.add_argument('--subset_to_label', help='# of images per camera to label', default = '10')
     args = parser.parse_args()
-
 
     # dir = glob.glob(f"{args.datapath}/**/*")  # /*") ## path to data directory
     dir = list(
         Path(args.datapath).rglob("*.JPG")
     )  # Recursively lists all files and directories
-
-    #dir = glob.glob(f"{args.datapath}/**/*")  # /*") ## path to data directory
-    dir = list(Path(args.datapath).rglob("*.JPG"))  # Recursively lists all files and directories
     dir = sorted(dir)
 
     ## labeling data
     filename = []
     PixelLengths = []
-    topX, topY, bottomX, bottomY = [], [], [], []
+    topX, topY, bottomX, bottomY = [],[],[],[]
     creationTimes = []
 
     ## customized data
@@ -67,8 +58,8 @@ def main():
     subset_to_label = np.int16(args.subset_to_label)
 
     ## some metadata data
-    cameraIDs = []
-    pole_lengths = []  ## tracks pole length
+    cameraIDs= []
+    pole_lengths = [] ## tracks pole length
     first_pole_pixel_length = []
     conversions = []
     widths, heights = [], []
@@ -106,9 +97,7 @@ def main():
 
     ### loop to label every nth photo!
     i = 0
-
     prev_cameraID = ""
-
     for j, file in tqdm.tqdm(enumerate(dir)):
         cameraID = Path(file).parent.name
         cameraIDs.append(cameraID)
@@ -130,7 +119,6 @@ def main():
         ##whether to start counter over
         i = i if len(cameraIDs) == 1 or cameraID == cameraIDs[-2] else 0
 
-
         if Path(file).name in filename:
             print(" ", Path(file).name, "has been labeled before, using stored data.")
 
@@ -140,50 +128,26 @@ def main():
             width, height, channel = img.shape
             ## assumes the cameras are stored in folder with their camera name
             plt.figure(figsize=(20, 10), num=Path(file).name)
-
-        if i % subset_to_label == 0:
-            img = cv2.imread(file)
-            width, height, channel = img.shape
-            ## assumes the cameras are stored in folder with their camera name
-            plt.figure(figsize=(20, 10))
-
             plt.imshow(img)
-            plt.title("label top and then bottom", fontweight="bold")
+            plt.title('label top and then bottom', fontweight = "bold")
             top, bottom = plt.ginput(2)
             topX.append(top[0]), topY.append(top[1])
             bottomX.append(bottom[0]), bottomY.append(bottom[1])
             plt.close()
 
-            PixelLength = math.dist(top, bottom)
+            PixelLength = math.dist(top,bottom)
             PixelLengths.append(PixelLength)
-
 
             ## save data to labels.csv
             nextline = f"\n{Path(file).name},{os.path.getctime(file)},{top[0]},{top[1]},{bottom[0]},{bottom[1]},{PixelLength}"
             with open(f"{args.datapath}/labels.csv", "a") as labels2_csv:
                 labels2_csv.write(nextline)
 
-
-            ## to get the pixel to centimeter conversion
-
-            if i == 0:
-                ## with the first photo, we will get some metadata
-                conversion = pole_length / PixelLength
-                ## and get metadata
-                first_pole_pixel_length.append(PixelLength)
-                conversions.append(conversion)
-                pole_lengths.append(pole_length)
-                heights.append(height), widths.append(width)
-
-            else:
-                pass
-
             filename.append(Path(file).name)
             creationTime = os.path.getctime(file)
             dt_c = datetime.datetime.fromtimestamp(creationTime)
             creationTimes.append(dt_c)
         i += 1
-
 
     ## simplified table for snow depth conversion later on
     metadata = pd.DataFrame({'camera_id':pd.unique(cameraIDs), 'pole_length_cm':pole_lengths,
@@ -192,36 +156,6 @@ def main():
     
     df.to_csv(f'{args.datapath}/labels.csv') 
     metadata.to_csv(f'{args.datapath}/pole_metadata.csv')
-
-        print(i)
-
-    df = pd.DataFrame(
-        {
-            "filename": filename,
-            "datetime": creationTimes,
-            "x1": topX,
-            "y1": topY,
-            "x2": bottomX,
-            "y2": bottomY,
-            "PixelLengths": PixelLengths,
-        }
-    )
-
-    ## simplified table for snow depth conversion later on
-    metadata = pd.DataFrame(
-        {
-            "camera_id": pd.unique(cameraIDs),
-            "pole_length_cm": pole_lengths,
-            "pole_length_px": (first_pole_pixel_length),
-            "pixel_cm_conversion": pd.unique(conversions),
-            "width": widths,
-            "height": heights,
-        }
-    )
-
-    df.to_csv(f"{args.datapath}/labels.csv")
-    metadata.to_csv(f"{args.datapath}/pole_metadata.csv")
-
 
 if __name__ == "__main__":
     main()
