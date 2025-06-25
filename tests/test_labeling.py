173e827f4e569f21b8e4335404de889ef83db5db
Nesitive <wbeau12@yahoo.com>
2025-06-25 15:44:04 -0500

    Clean up labels.csv if it wasn't there before

--- tests/test_labeling.py
+++ tests/test_labeling.py
@@ -13,29 +13,31 @@ import labeling
 
 class LabelingTest(unittest.TestCase):
     def setUp(self):
-        if (os.path.exists("tests/data/labels.csv")):
+        if os.path.exists("tests/data/labels.csv"):
             os.rename("tests/data/labels.csv", "tests/data/labels.old")
-        if (os.path.exists("tests/data/pole_metadata.csv")):
+        if os.path.exists("tests/data/pole_metadata.csv"):
             os.rename("tests/data/pole_metadata.csv", "tests/data/pole_metadata.old")
 
     def test_labeling(self):
         labeling.label_photos("tests/data", 150, 6)
         self.assertTrue(os.path.exists("tests/data/labels.csv"))
         self.assertTrue(os.path.exists("tests/data/pole_metadata.csv"))
-        if not os.path.exists("tests/data/labels.old"):
-            copyfile("tests/data/labels.csv", "tests/data/labels.old")
-        if not os.path.exists("tests/data/pole_metadata.old"):
-            copyfile("tests/data/pole_metadata.csv", "tests/data/pole_metadata.old")
+        copyfile("tests/data/labels.csv", "tests/data/labels.old")
+        copyfile("tests/data/pole_metadata.csv", "tests/data/pole_metadata.old")
 
     def test_labeling_autosave(self):
         copyfile("tests/data/labels.old", "tests/data/labels.csv")
         copyfile("tests/data/pole_metadata.old", "tests/data/pole_metadata.csv")
         labeling.label_photos("tests/data", 150, 6)
         self.assertTrue(filecmp.cmp("tests/data/labels.csv", "tests/data/labels.old"))
-        self.assertTrue(filecmp.cmp("tests/data/pole_metadata.csv", "tests/data/pole_metadata.old"))
+        self.assertTrue(
+            filecmp.cmp("tests/data/pole_metadata.csv", "tests/data/pole_metadata.old")
+        )
 
     def tearDown(self):
         os.remove("tests/data/labels.csv")
         os.remove("tests/data/pole_metadata.csv")
-        os.rename("tests/data/labels.old", "tests/data/labels.csv")
-        os.rename("tests/data/pole_metadata.old", "tests/data/pole_metadata.csv")
+        if os.path.exists("tests/data/labels.old"):
+            os.rename("tests/data/labels.old", "tests/data/labels.csv")
+        if os.path.exists("tests/data/pole_metadata.old"):
+            os.rename("tests/data/pole_metadata.old", "tests/data/pole_metadata.csv")
