c6f9c1b3ca4637a55376bb42ada244b31921f25e
Nesitive <wbeau12@yahoo.com>
2025-06-25 13:57:52 -0500

    Add tests for no data in labels.csv and data already in labels.csv

--- tests/test_labeling.py
+++ tests/test_labeling.py
@@ -11,7 +11,16 @@ import labeling
 
 
 class LabelingTest(unittest.TestCase):
+    def setUp(self):
+        os.rename("tests/data/labels.csv", "tests/data/labels.old")
+        os.rename("tests/data/pole_metadata.csv", "tests/data/pole_metadata.old")
+
     def test_labeling(self):
+        labeling.label_photos("tests/data", 150, 6)
+        self.assertTrue(os.path.exists("tests/data/labels.csv"))
+        self.assertTrue(os.path.exists("tests/data/pole_metadata.csv"))
+
+    def test_labeling_autosave(self):
         labeling.label_photos("tests/data", 150, 6)
         self.assertTrue(os.path.exists("tests/data/labels.csv"))
         self.assertTrue(os.path.exists("tests/data/pole_metadata.csv"))
@@ -19,3 +28,5 @@ class LabelingTest(unittest.TestCase):
     def tearDown(self):
         os.remove("tests/data/labels.csv")
         os.remove("tests/data/pole_metadata.csv")
+        os.rename("tests/data/labels.old", "tests/data/labels.csv")
+        os.rename("tests/data/pole_metadata.old", "tests/data/pole_metadata.csv")
