6147c575b296500c03fecb3e205ee37d4df38b67
Nesitive <wbeau12@yahoo.com>
2025-06-25 15:29:01 -0500

    Fix all FileNotFoundErrors (attempt 1)

--- tests/test_labeling.py
+++ tests/test_labeling.py
@@ -22,18 +22,20 @@ class LabelingTest(unittest.TestCase):
         labeling.label_photos("tests/data", 150, 6)
         self.assertTrue(os.path.exists("tests/data/labels.csv"))
         self.assertTrue(os.path.exists("tests/data/pole_metadata.csv"))
+        if not os.path.exists("tests/data/labels.old"):
+            os.rename("tests/data/labels.csv", "tests/data/labels.old")
+        if not os.path.exists("tests/data/pole_metadata.old"):
+            os.rename("tests/data/pole_metadata.csv", "tests/data/pole_metadata.old")
 
     def test_labeling_autosave(self):
-        copyfile("tests/data/labels.csv", "tests/data/labels.csv.001")
-        copyfile("tests/data/pole_metadata.csv", "tests/data/pole_metadata.csv.001")
+        copyfile("tests/data/labels.old", "tests/data/labels.csv")
+        copyfile("tests/data/pole_metadata.old", "tests/data/pole_metadata.csv")
         labeling.label_photos("tests/data", 150, 6)
-        self.assertTrue(filecmp("tests/data/labels.csv", "tests/data/labels.csv.001"))
-        self.assertTrue(filecmp("tests/data/pole_metadata.csv", "tests/data/pole_metadata.csv.001"))
+        self.assertTrue(filecmp("tests/data/labels.csv", "tests/data/labels.old"))
+        self.assertTrue(filecmp("tests/data/pole_metadata.csv", "tests/data/pole_metadata.old"))
 
     def tearDown(self):
         os.remove("tests/data/labels.csv")
         os.remove("tests/data/pole_metadata.csv")
-        if (os.path.exists("tests/data/labels.old")):
-            os.rename("tests/data/labels.old", "tests/data/labels.csv")
-        if (os.path.exists("tests/data/pole_metadata.old")):
-            os.rename("tests/data/pole_metadata.old", "tests/data/pole_metadata.csv")
+        os.rename("tests/data/labels.old", "tests/data/labels.csv")
+        os.rename("tests/data/pole_metadata.old", "tests/data/pole_metadata.csv")
