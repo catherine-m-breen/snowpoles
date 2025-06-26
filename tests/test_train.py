c01f4996ed4cac8aa856ca73c2701753cc4405e1
Nesitive <wbeau12@yahoo.com>
2025-06-26 16:03:10 -0500

    Remove unneeded import

--- tests/test_train.py
+++ tests/test_train.py
@@ -3,7 +3,6 @@ import os
 from shutil import copyfile
 import sys
 import unittest
-from unittest.mock import patch
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/src")
 
