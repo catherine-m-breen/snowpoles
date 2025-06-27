4b02f7be4b5faa2fe318050a69afc129fd119a45
Nesitive <wbeau12@yahoo.com>
2025-06-27 10:44:42 -0500

    Fix IndexError and IndentationError

--- src/labeling.py
+++ src/labeling.py
@@ -113,20 +113,6 @@ def main():
         cameraID = Path(file).parent.name
         cameraIDs.append(cameraID)
 
-        if not len(prev_cameraID) or cameraID != prev_cameraID:
-            prev_cameraID = cameraID
-            mj = int(j / subset_to_label)
-            PixelLength = math.dist((float(topX[mj]), float(topY[mj])), (float(bottomX[mj]), float(bottomY[mj])))
-            ## with the first photo, we will get some metadata
-            conversion = pole_length / PixelLength
-            ## and get metadata
-            first_pole_pixel_length.append(PixelLength)
-            conversions.append(conversion)
-            pole_lengths.append(pole_length)
-            img = cv2.imread(file)
-            width, height, channel = img.shape
-            heights.append(height), widths.append(width)
-
         ##whether to start counter over
         i = i if len(cameraIDs) == 1 or cameraID == cameraIDs[-2] else 0
 
@@ -182,6 +168,21 @@ def main():
             creationTime = os.path.getctime(file)
             dt_c = datetime.datetime.fromtimestamp(creationTime)
             creationTimes.append(dt_c)
+
+        if not len(prev_cameraID) or cameraID != prev_cameraID:
+            prev_cameraID = cameraID
+            mj = int(j / subset_to_label)
+            PixelLength = math.dist((float(topX[mj]), float(topY[mj])), (float(bottomX[mj]), float(bottomY[mj])))
+            ## with the first photo, we will get some metadata
+            conversion = pole_length / PixelLength
+            ## and get metadata
+            first_pole_pixel_length.append(PixelLength)
+            conversions.append(conversion)
+            pole_lengths.append(pole_length)
+            img = cv2.imread(file)
+            width, height, channel = img.shape
+            heights.append(height), widths.append(width)
+
         i += 1
 
 
@@ -192,8 +193,6 @@ def main():
     
     df.to_csv(f'{args.datapath}/labels.csv') 
     metadata.to_csv(f'{args.datapath}/pole_metadata.csv')
-
-        print(i)
 
     df = pd.DataFrame(
         {
